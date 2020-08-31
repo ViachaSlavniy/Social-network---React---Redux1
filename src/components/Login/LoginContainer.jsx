3ae6f8d8ca31bec6bab76ce9e2525536714b4259
Viachaslav Smolenski <simpleray22@gmail.com>
2020-08-31 18:44:22 +0300

    Lesson 87. Use Hooks useState, useEffect

---
+++ src/components/Login/LoginContainer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Field, reduxForm} from 'redux-form'
+
+
+const LoginForm = (props) => {
+    const { handleSubmit } = props
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component='input'/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component='input'/>
+            </div>
+            <div>
+                <Field type={'checkbox'} name={'rememberMe'} component='input'/>Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+const Login = (props) => {
+    let onSubmit = (formData) => {
+        console.log(formData);
+    }
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
+    )
+}
+
+
+export default Login;
---
+++ src/redux/app-reducer.js
@@ -0,0 +1,92 @@
+import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
+
+const SET_AUTH_USER_DATA = 'SET_AUTH_USER_DATA';
+const SET_CAPTCHA = 'SET_CAPTCHA';
+
+const initialState = {
+    userId: null,
+    login: null,
+    email: null,
+    isAuth: false,
+    captchaUrl: null
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_AUTH_USER_DATA: {
+            return {
+                ...state,
+                ...action.data
+            }
+        }
+        case SET_CAPTCHA: {
+            return {
+                ...state,
+                ...action.data
+            }
+        }
+        default:
+            return state;
+    }
+
+}
+
+// Создаем ActionCreators
+
+export let setAuthUserData = (userId, login, email, isAuth) => {
+    return {type: SET_AUTH_USER_DATA, data: {userId, login, email, isAuth}}
+}
+export let setCaptcha = (captchaUrl) => {
+    return {type: SET_AUTH_USER_DATA, data: {captchaUrl}}
+}
+
+// Создаем ThunkCreators
+
+export let getAuthUserData = () => {
+    return (dispatch) => {
+        authAPI.me()
+            .then(data => {
+            if(data.resultCode === 0) {
+                let {id, login, email} = data.data;
+                dispatch(setAuthUserData(id, login, email, true));
+            }
+        });
+    }
+}
+
+export let login = (email, password, rememberMe, captcha) => {
+    return (dispatch) => {
+        authAPI.login(email, password, rememberMe, captcha)
+            .then(response => {
+                if(response.data.resultCode === 0) {
+                    dispatch(getAuthUserData())
+                }
+                if(response.data.resultCode === 1) {
+                    let message = response.data.messages ? response.data.messages[0] : 'Some error';
+                    dispatch(stopSubmit('login',{_error: message}));
+                }
+                if(response.data.resultCode === 10) {
+                    authAPI.getCaptchaUrl()
+                        .then(response => {
+                            dispatch(setCaptcha(response.url));
+                        })
+                    let message = response.data.messages ? response.data.messages[0] : 'Some error';
+                    dispatch(stopSubmit('login',{_error: message}));
+                }
+            });
+    }
+}
+
+export let logout = () => {
+    return (dispatch) => {
+        authAPI.logout()
+            .then(response => {
+                if(response.data.resultCode === 0) {
+                    dispatch(setAuthUserData(null, null, null, false));
+                }
+            });
+    }
+}
+
+export default authReducer;
