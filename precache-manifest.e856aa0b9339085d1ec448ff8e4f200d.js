b75a409a5c630d9fa2af27598bb2600bacfe3867
Viachaslav Smolenski <simpleray22@gmail.com>
2020-09-14 14:14:05 +0300

    Updates

---
+++ precache-manifest.e856aa0b9339085d1ec448ff8e4f200d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cef83231b85f17c24c089dae1e3d181",
+    "url": "/Social-network---React---Redux1/index.html"
+  },
+  {
+    "revision": "39230edaaf07a5b3bb6f",
+    "url": "/Social-network---React---Redux1/static/css/3.e04537db.chunk.css"
+  },
+  {
+    "revision": "9a1fc858722560876bcd",
+    "url": "/Social-network---React---Redux1/static/css/main.30352b0f.chunk.css"
+  },
+  {
+    "revision": "da7ebae545e5327a406d",
+    "url": "/Social-network---React---Redux1/static/js/2.9323f661.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Social-network---React---Redux1/static/js/2.9323f661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39230edaaf07a5b3bb6f",
+    "url": "/Social-network---React---Redux1/static/js/3.f4f121d9.chunk.js"
+  },
+  {
+    "revision": "9a1fc858722560876bcd",
+    "url": "/Social-network---React---Redux1/static/js/main.11b5ae08.chunk.js"
+  },
+  {
+    "revision": "39341cd239b059c8b998",
+    "url": "/Social-network---React---Redux1/static/js/runtime-main.660044f6.js"
+  },
+  {
+    "revision": "e1c23af8e2642f2478f6dea87592a965",
+    "url": "/Social-network---React---Redux1/static/media/preloader.e1c23af8.svg"
+  },
+  {
+    "revision": "1e976c46d0c8de419b7645c0a1835d7c",
+    "url": "/Social-network---React---Redux1/static/media/unknown-user.1e976c46.png"
+  }
+]);
